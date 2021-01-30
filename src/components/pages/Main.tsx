2ce578d6d97a7e8415221070005f4e96b5990054
嶋津温紀 <160441081@ccalumni.meijo-u.ac.jp>
2021-01-30 19:06:16 +0900

    createData

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import apiKey from "../../apiKey";
+import { population, prefectures } from "../../data";
 import Graph from "../organisms/Graph";
 
 const Main: React.FC = () => {
   const [count, setCount] = useState(0);
   const [value, setValue] = useState("initial");
+
+  console.log(population, prefectures);
 
   useEffect(() => {
     // この副作用は変数valueを使う
